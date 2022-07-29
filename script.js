e1ef692e16ec01337430e3826e4e766307995b7b
LauraMontano <lauramotano3414@gmail.com>
2022-07-29 17:22:47 -0300

    Correcion de desafio clase 7

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
     3:Editar perfil medico 
     4.Ver mis notas rapidas   
     5:Agregar mascota
+    6.Editar datos del usuario
     0:Salir
     Otro valor: vuelve a preguntar
 */
@@ -15,17 +16,23 @@ class Usuario{
         this.user = user;
         this.password = psw;
     }
-    cambiarUsuario(){
-        let nuevoUsuario = prompt("Ingrese nuevo usuario");
-        this.usuario = nuevoUsuario;
-    }
-    cambiarPsw(){
-        let nuevaPsw = prompt("Ingrese anterior contraseña");
-        if(nuevaPsw = this.password){
-            nuevaPsw = prompt("Ingrese nueva constraseña");
-            this.password = nuevaPsw
-        }else{
-            alert("Contraseña invalida");
+    cambiarDatos(op){
+        switch(op){
+            case('1'):
+                let nuevoUsuario = prompt("Ingrese nuevo usuario");
+                this.usuario = nuevoUsuario;
+                break;
+            case('2'):
+                let nuevaPsw = prompt("Ingrese anterior contraseña");
+                if(nuevaPsw = this.password){
+                    nuevaPsw = prompt("Ingrese nueva constraseña");
+                    this.password = nuevaPsw
+                }else{
+                    alert("Contraseña invalida");
+                }
+                break;
+            default:alert("Opcion invalida\n");
+                break;
         }
     }
 }
@@ -49,28 +56,26 @@ class Mascota{
             this.bitacora.push(bitacora) ;
         }
     }
-    CambiarNombre(){
-        this.nombre = prompt("Ingrese nuevo nombre: ");
+    CambiarDato(opcion){
+        switch(opcion){
+            case ('1'):this.nombre = prompt("Ingrese nuevo nombre: ");
+                break;
+            case ('2'):this.raza = prompt("Ingrese raza: ");
+                break;
+            case ('3'):this.edad = prompt("Ingrese edad: ");
+                break;
+            case ('4'):this.peso = prompt("Ingrese peso: ");
+                break;
+            case ('5'):this.bitacora.push(prompt("Ingrese nueva bitacora: "));
+                break;
+            case ('6'):this.vacunas.push(prompt("Ingrese nueva vacuna: "));
+                break;
+            case ('7'):this.especie = prompt("Ingrese especie: ");
+                break;
+            default: alert("Opcion invalida\n");
+                break;
+        }
     }
-    CambiarEspecie(){
-        this.especie = prompt("Ingrese especie: ");
-    }
-    CambiarPeso(){
-        this.peso = prompt("Ingrese peso: ");
-    }
-    CambiarEdad(){
-        this.edad = prompt("Ingrese edad: ");
-    }
-    CambiarRaza(){
-        this.raza = prompt("Ingrese raza: ");
-    }
-    AgregarBitacora(){
-        this.bitacora.push(prompt("Ingrese nueva bitacora: "));
-    }
-    AgregarVacunas(){
-        this.vacunas.push(prompt("Ingrese nueva vacuna: "));
-    }
-
     MostrarPerfil(){
         alert("Perfil de " + this.nombre + "\nraza: " + this.raza + "\nedad: " + this.edad + "años" + "\npeso:" +this.peso + "kg" + "\nvacunas: " +this.vacunas.join('\n')+ "\nbitacora: " +this.bitacora.join('\n'));
     }
@@ -81,18 +86,20 @@ class Mascota{
 function agregarMascota() {
     const nombre = prompt("Ingresa el nombre:");
     const especie = prompt("Ingresar especie:");
+    const raza = prompt("Ingresar raza (si es desconocida ingrese none):");
     const edad = prompt("Ingresar edad:");
     const peso = prompt("Ingresar peso: ");
-
-    return new Mascota(nombre, especie, edad, peso);
+    return new Mascota(nombre, especie, edad, peso, raza);
 }
 
 function elegirMascota(Mascotas){
     let texto = " ";
-    for(let index = 0 ; index <Mascotas.length ; index++){
-        texto += (index+1) + ":" + Mascotas[index].nombre + "\n";
-    }
-    return parseInt(prompt("Ingrese el indice de la mascota deseada\n" + texto))-1;
+    let index = 0;
+    Mascotas.forEach(element => {
+        index++;
+        texto += index + ":" + element.nombre + "\n";
+    });
+    return parseInt(prompt("Ingrese el indice de la mascota deseada\n" + texto));
 
 }
 
@@ -109,7 +116,7 @@ function imprimirTexto(texto){
 let opcion = '1';
 let notaRapida = [];
 let usuario = new Usuario ("LauraMontaño" , "Laura1234");
-let mascotas = [new Mascota("Sasha", "especie", 5, 10)];2
+let mascotas = [new Mascota("Sasha", "especie", 5, 10, "golden")];
 /* Fin de Variables */
 for(let i = 5; i>0 ; i--){
     let user = prompt("Ingrese usuario: ");
@@ -117,39 +124,35 @@ for(let i = 5; i>0 ; i--){
 
     if((user === usuario.user) && (psw === usuario.password)){
         while(opcion !== '0'){
-            opcion = prompt("Ingrese opcion deseada: \n1:Ver perfil medico de mis mascotas \n2:Hacer una nota rapida \n3:Editar perfil medico \n4:Ver mis notas rapidas \n5:Agregar nueva mascota\n0:Salir  ");
+            opcion = prompt("Ingrese opcion deseada: \n1:Ver perfil medico de mis mascotas \n2:Hacer una nota rapida \n3:Editar perfil medico \n4:Ver mis notas rapidas \n5:Agregar nueva mascota\n6:Editar Informacion del usuarion\n0:Salir  ");
             switch(opcion){
                 case('0'):console.log("Usuario cerro sesión");
                     break;
-                case('1'):
-                    for (const key of mascotas) {
-                        key.MostrarPerfil();
-                    }
+                case('1'):mascotas.forEach(element => {element.MostrarPerfil();});
                     break;
-                case('2'):let notaNueva = prompt("Ingrese nota rapida: ") ;
-                    notaRapida.push(notaNueva);
+                case('2'):notaRapida.push(prompt("Ingrese nota rapida: "));
                     break;
                 case('3'):
                     let indice = elegirMascota(mascotas);
-                    let _opcion = prompt("Ingrese una opcion: \n1:Editar nombre.\n2:Editar raza.\n3:Editar edad.\n4:Editar peso.\n5:Editar bitacora.\n6:Editar vacunas.");
-                    switch(_opcion){
-                        case ('1'):mascotas[indice].CambiarNombre();
-                            break;
-                        case ('2'):mascotas[indice].CambiarRaza();
-                            break;
-                        case ('3'):mascotas[indice].CambiarEdad(); 
-                            break;
-                        case ('4'):mascotas[indice].CambiarPeso(); 
-                            break;
-                        case ('5'):mascotas[indice].AgregarBitacora(); 
-                            break;
-                        case ('6'):mascotas[indice].AgregarVacunas();
-                            break;
-                    }
+                    let _opcion = prompt("Ingrese una opcion: \n1:Editar nombre.\n2:Editar raza.\n3:Editar edad.\n4:Editar peso.\n5:Editar bitacora.\n6:Editar vacunas.\n7:Editar especie");
+                    mascotas[indice].CambiarDato(_opcion);
                     break;
                 case('4'):imprimirTexto(notaRapida);
                     break;
                 case('5'):mascotas.push(agregarMascota()) ;
+                    mascotas.sort((a, b) => {//Ordena por orden alfabetico la lista de mascotas
+                        if (a.nombre.toUpperCase() > b.nombre.toUpperCase()) {
+                            return 1;
+                        }
+                        if (a.nombre.toUpperCase() < b.nombre.toUpperCase()) {
+                            return -1;
+                        }
+                        // a es igual a b
+                        return 0;
+                    }) 
+                    break;
+                case('6'):let op = prompt("Ingrese opcion: \n1:Cambiar nombre.\n2:Cambiar contraseña");
+                    usuario.cambiarDatos(op);
                     break;
                 default: alert("Opcion Invalida.");
                     break;
